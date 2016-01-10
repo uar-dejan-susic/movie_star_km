ef876da3877d04483cc9be4728cd82b2da5154be
kemal <kemal.muhovic@upandrunningsoftware.com>
2016-01-10 15:20:40 +0000

    initial rails skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MovieStarKm::Application.config.secret_key_base = '32d557453bd7f46124d23fac19d9ebe403beb9c1de6eb5e89cc75173b7653fd257c30d0bec79aa56a7e48c6ac45e361669e5ce2a95c31945237e13377db3e460'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MovieStarKm::Application.config.session_store :cookie_store, key: '_movie_star_km_session'
