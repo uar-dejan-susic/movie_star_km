88ddc57fe106732c0e27b6f064dbdf2e9a80b355
kemalmuhovic <kemal.muhovic@upandrunningsoftware.com>
2016-01-23 17:04:23 +0000

    Ticket #178: Cover parts of the movie star application with specs -  writing tests for home controller

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -7,6 +7,12 @@ RSpec.describe HomeController, type: :controller do
       get :index
       expect(response).to have_http_status(:success)
     end
+
+    it 'redirects to the "movies index" action for the authenticated user' do
+      login_with create( :user )
+      get :index
+      expect(response).to redirect_to movies_path
+    end
   end
 
 end
