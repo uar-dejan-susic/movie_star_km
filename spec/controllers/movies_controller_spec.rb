0a8598ec6a5e2a0d6104a4a550c7b44adfa1eb25
kemalmuhovic <kemal.muhovic@upandrunningsoftware.com>
2016-01-23 15:20:27 +0000

    Ticket #178: Cover parts of the movie star application with specs - setting up controller helper for authentication in tests, writing tests for movies controller

--- spec/controllers/movies_controller_spec.rb
+++ spec/controllers/movies_controller_spec.rb
@@ -2,11 +2,23 @@ require 'rails_helper'
 
 RSpec.describe MoviesController, type: :controller do
 
-  describe "GET #edit" do
-    it "returns http success" do
-      get :edit
-      expect(response).to have_http_status(:success)
+  describe 'anonymous user' do
+    before :each do
+      # This simulates an anonymous user
+      login_with nil
+    end
+
+    it 'should be redirected to signin' do
+      get :index
+      expect( response ).to redirect_to( new_user_session_path )
     end
   end
+
+  # describe "GET #edit" do
+  #   it "returns http success" do
+  #     get :edit
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
 
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,6 +16,10 @@
 # users commonly want.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+
+require_relative 'support/controller_helpers'
+require 'devise'
+
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
@@ -38,6 +42,13 @@ RSpec.configure do |config|
     # a real object. This is generally recommended, and will default to
     # `true` in RSpec 4.
     mocks.verify_partial_doubles = true
+  end
+
+  config.include ControllerHelpers, type: :controller
+  Warden.test_mode!
+
+  config.after do
+    Warden.test_reset!
   end
 
 # The settings below are suggested to provide a good initial experience
